36e2cccea8c082a6f06c34debe3b2fe625477d4c
javigondeveloper <javiergonzalez.webdeveloper@gmail.com>
2023-06-15 13:16:43 +0200

    Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Layout({ title, children }) {
   return (
@@ -7,10 +8,26 @@ export default function Layout({ title, children }) {
         <title>{title ? title : 'Food Cart'}</title>
         <meta name="description" content="Ecommerce Website" />
       </Head>
-      <div>
-        <header>Header</header>
-        <main>{children}</main>
-        <footer>Footer</footer>
+      <div className=" flex min-h-screen flex-col justify-between">
+        <header>
+          <nav className=" flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/" className="text-lg font-bold">
+              Food Cart
+            </Link>
+            <div>
+              <Link href="/cart" className=" p-2 text-lg">
+                Cart
+              </Link>
+              <Link href="/login" className="p-2 text-lg">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          Copyright © 2023 Javi González
+        </footer>
       </div>
     </>
   );
